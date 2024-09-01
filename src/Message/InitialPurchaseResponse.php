5f5da52b385976c84d18ce702a8ed63bf8ac7e71
Dimitar Dishev <dimitar.dishev@ampeco.global>
2024-09-01 13:12:49 +0300

    add: pending check and message extract on the strange "initial purchase response" class

--- src/Message/InitialPurchaseResponse.php
+++ src/Message/InitialPurchaseResponse.php
@@ -9,6 +9,11 @@ class InitialPurchaseResponse extends Response
         return parent::isSuccessful() && in_array(@$this->data['payment']['status'], [self::STATUS_REDIRECTED, self::STATUS_PENDING_CAPTURE]);
     }
 
+    public function isPending(): bool
+    {
+        return in_array(@$this->data['payment']['status'], [self::STATUS_CREATED]);
+    }
+
     public function getRedirectUrl()
     {
         return $this->data['merchantAction']['redirectData']['redirectURL'] ?? null;
@@ -17,5 +22,19 @@ class InitialPurchaseResponse extends Response
     public function getTransactionReference(): ?string
     {
         return $this->data['payment']['id'] ?? null;
+    }
+
+    public function getMessage()
+    {
+        if (isset($this->data['errors'])) {
+            return implode(', ', array_map(fn ($error) => $error['id'] . ':' . ($error['errorCode'] ?? '') . ($error['message'] ?? ''), $this->data['errors']));
+        }
+
+        return '';
+    }
+
+    public function getCode()
+    {
+        return @$this->data['errors'][0]['errorCode'] ?? parent::getCode();
     }
 }
--- src/Message/Response.php
+++ src/Message/Response.php
@@ -7,10 +7,17 @@ use Omnipay\Common\Message\RequestInterface;
 
 class Response extends AbstractResponse
 {
-    const STATUS_CAPTURED = 'CAPTURED';
-    const STATUS_PENDING_CAPTURE = 'PENDING_CAPTURE';
-    const STATUS_CAPTURE_REQUESTED = 'CAPTURE_REQUESTED';
+    const STATUS_CREATED = 'CREATED';
+    const STATUS_CANCELLED = 'CANCELLED';
+    const STATUS_REJECTED = 'REJECTED';
+    const STATUS_REJECTED_CAPTURE = 'REJECTED_CAPTURE';
     const STATUS_REDIRECTED = 'REDIRECTED';
+    const STATUS_PENDING_CAPTURE = 'PENDING_CAPTURE';
+    const STATUS_AUTHORIZATION_REQUESTED = 'AUTHORIZATION_REQUESTED';
+    const STATUS_CAPTURE_REQUESTED = 'CAPTURE_REQUESTED';
+    const STATUS_CAPTURED = 'CAPTURED';
+    const STATUS_REFUND_REQUESTED = 'REFUND_REQUESTED';
+    const STATUS_REFUNDED = 'REFUNDED';
 
     public function __construct(RequestInterface $request, $data, protected int $code)
     {
