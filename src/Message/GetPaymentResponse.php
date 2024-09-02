bff0c8e0cde092cde10cd8889dedcd2a9c5e9b73
Valentin Damyanov <valentin.damyanov@ampeco.global>
2024-09-02 10:26:06 +0300

    Update version and add card details getters

--- src/Message/GetPaymentResponse.php
+++ src/Message/GetPaymentResponse.php
@@ -8,4 +8,20 @@ class GetPaymentResponse extends Response {
     {
         return parent::isSuccessful() && $this->data['status'] === self::STATUS_PENDING_CAPTURE;
     }
+
+    public function getToken()
+    {
+        return $this->data['paymentOutput']['cardPaymentMethodSpecificOutput']['token'];
+    }
+
+    public function getCardNumber()
+    {
+        return @$this->data['paymentOutput']['cardPaymentMethodSpecificOutput']['card']['cardNumber'];
+    }
+
+    public function getExpiryDate()
+    {
+        return @$this->data['paymentOutput']['cardPaymentMethodSpecificOutput']['card']['expiryDate'];
+    }
+
 }
