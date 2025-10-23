c3a9f93b8811a5e6a7c45fc340c59e305f7cf8f1
Valentin Damyanov <valentin.damyanov@ampeco.global>
2025-10-23 10:17:55 +0300

    bump version to 1.5.7 in composer.json and add getTransactionReference method to VoidResponse

--- src/Message/VoidResponse.php
+++ src/Message/VoidResponse.php
@@ -8,4 +8,9 @@ class VoidResponse extends Response {
     {
         return parent::isSuccessful() && @$this->data['payment']['status'] === self::STATUS_CANCELLED;
     }
+
+    public function getTransactionReference(): ?string
+    {
+        return $this->data['id'] ?? parent::getTransactionReference();
+    }
 }
